28662cee20510b7d20cda68ec8522c3eada2460e
Al Hasan <alhasan375935@gmail.com>
2024-02-16 16:13:25 +0600

    alpha clash pro near  last stage commit

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,9 +11,82 @@
 
 // }
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    // jodi game over korte chai kelar moddyi tahole nirdisto ekta button e click korle stop hoye jabe game
+if(playerPressed === 'Escape'){
+    gameOver();
+}
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expectedAlphabet);
+
+    if(playerPressed === expectedAlphabet){
+        console.log('you got a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        // console.log(currentScore);
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+
+
+
+// protibar correct key te press korar por ekta kore score barbe seita eikane korbo
+// const currentScoreElement = document.getElementById('current-score');
+// const currentScoreText = currentScoreElement.innerText;
+// // string hisabe score ta k number e convert korte hobe karon ei score ta jog hobe
+// const currentScore = parseInt(currentScoreText);
+// console.log(currentScoreText);
+
+
+
+// // score ta k jog korbo
+const newScore = currentScore + 1;
+
+// currentScoreElement.innerText = newScore;
+
+// uporer function ta commment kore raklam  kichota vinno vave same kaj korbo (1)
+
+        // console.log('you have pressed correctly ' , expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        containueGame();
+    }
+    else{
+        console.log('you missed a point and lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife -1;
+        setTextElementValueById('current-life', updatedLife);
+
+        // jodi life  5 tar er theke kom hoye jai tokon game over hoye jave and next page a chole jabe
+        if(updatedLife === 0){
+            gameOver();
+        }
+        // jodi vul kore type koro tahole eitar jonno sojog ba life dewa hobe  5 bar
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        
+        
+        // const newLife = currentLife -1;
+
+        // currentLifeElement.innerText = newLife;
+        // // uporer function ta commment kore raklam  kichota vinno vave same kaj korbo (2)
+
+    }
+}
+document.addEventListener('keyup' , handleKeyboardButtonPress);
+
+
 function containueGame(){
     const alphabet = getARandomAlphabet();
-    console.log( 'Your random alphabet', alphabet);
+    // console.log( 'Your random alphabet', alphabet);
 
     // kono ekta paragraph upadan k array moddy dekanor jonno oi paragraph er id ta k nia aste hobe
 
@@ -26,6 +99,27 @@ function containueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+// game over er pore jokon nuton kore game start korte jabo tokon life score abaro 5 add hoye jabe
+setTextElementValueById('current-life', 5);
+// abaro jokon again game ta start korbo tokon score er maan taw 0 takbe
+setTextElementValueById('current-score', 0);
+
     containueGame()
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    // update final score and get last page show the score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score', lastScore);
+
+    // clear the last selected key bg color
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
 }
--- scripts/second-alpha.js
+++ scripts/second-alpha.js
@@ -16,6 +16,30 @@ function addBackgroundColorById(elementId){
     element.classList.add('bg-amber-500');
 }
 
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-amber-500');
+}
+
+// vinno vave score count and life minus korar kaj ta korchi function dia
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+const element = document.getElementById(elementId);
+element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 function getARandomAlphabet(){
     const alphabetString = 'egbeacbdkelmonwnqpritswz';
     // kono ekta string maan k array korar jonno variable ta sathy . dot split kore faka bracket dibo ex: ('')
@@ -26,6 +50,7 @@ function getARandomAlphabet(){
     // get a random index between 
     // Math.random( ) eita use korle array er moddy index number 0-1 er moddy number gula k dekabe and sei sathy jodi 23 dia gon kori tahle 23 ta number takle sei prottekta upadan k amk dekabe 
     const randomNumber = Math.random()*23;
+    // Math.round ekti float wala number k purno shongkai convert korte pare 
     const index = Math.round(randomNumber);
 
 
